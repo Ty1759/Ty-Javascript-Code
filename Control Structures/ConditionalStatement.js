8ba8220cd8f7987914f459bdab7930f2bd62f171
Ty1759 <oyelekanoyebisi@gmail.com>
2024-11-12 14:35:20 +0100

    Worked on switch statement

--- Control Structures/ConditionalStatement.js
+++ Control Structures/ConditionalStatement.js
@@ -35,3 +35,57 @@ if (hasPaidSchoolFees && hasGoodHealth) {
 } else {
     console.log("Go back home")
 }
+
+
+let age = 25;
+if (age > 25){
+    console.log("your age is above 25")
+} else if(age < 25){
+    console.log("your age is below 25")
+} else {
+    console.log ("your age is 25")
+}
+
+let fruit = "apple";
+if(fruit === "banana") {
+    console.log( 'we have banana in the store')
+} else if(fruit === "orange"){
+    console.log ("we have orange in our store")
+} else if(fruit === "coconut"){
+    console.log("coconut is in our store")
+} else if(fruit === "apple"){
+    console.log ("we have apple in our store")
+} else{
+    console.log ("we do not have any fruit")
+}
+
+let fruit1 = "apple";
+
+// Switch statement
+switch (fruit1) {
+    case "banana":
+        console.log ("it is banana")
+        break;
+
+        case "apple":
+            console.log ("it is apple")
+            break;
+
+        default:
+            console.log("we do not have any fruit")
+
+}
+
+let age1 = 25
+switch (age1) {
+    case age1 > 25:
+        console.log ("your age is above 25");
+        break;
+
+        case age1 < 25:
+            console.log ("your age is below 25");
+            break;
+
+        default:
+            console.log("your age is 25")
+}
