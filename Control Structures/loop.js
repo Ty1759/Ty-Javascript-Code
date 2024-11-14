1bf544492e48d2370cb5c134213d98e46dc42647
Ty1759 <oyelekanoyebisi@gmail.com>
2024-11-14 13:08:56 +0100

    Made changes the script

---
+++ Control Structures/loop.js
@@ -0,0 +1,11 @@
+//Loops are used to execute a block of code repeatedly until specific condtions are met
+
+for (let i = 1; i < 11; i++) {
+    console.log("hello from Loop" + i);
+};
+
+const colors = ["red", "blue", "green", "yellow"]
+
+for (let i = 0; i < colors.length; i++){
+    console.log("Color:", colors[i]);
+}
