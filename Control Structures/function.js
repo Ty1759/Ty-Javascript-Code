85370850cfe23412fb90eb43a9487e51bfee453c
Ty1759 <oyelekanoyebisi@gmail.com>
2024-11-19 13:02:03 +0100

    Function Javascript

---
+++ Control Structures/function.js
@@ -0,0 +1,12 @@
+// function
+// function is a block of code that performs an actions and is reusable.
+
+//types of function
+// function declaration
+//function expression
+
+// 1. function declaration
+    function add(a, b){
+        return(a+b)
+    }
+    add(2,4)
